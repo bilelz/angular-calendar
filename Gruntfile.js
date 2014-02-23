cbce6efc60f22231f91bab6e927b230dd3d8942e
bilelz <bilelz@gmail.com>
2014-02-23 22:26:40 +0100

    blablabla

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,10 +64,20 @@ module.exports = function(grunt) {
 		            spawn: false,
 		        },
 		    } 
-		}
+		},
+		copy: {
+		  main: {
+		    files: [
+		      // includes files within path
+		      {expand: true, flatten: true, src: ['js/libs/font-awesome/font/*'], dest: 'build/css/font/', filter: 'isFile'}
+		    ]
+		  }
+	}
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
@@ -77,6 +87,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', [ 'uglify', 'cssmin','processhtml', 'htmlmin']);
+    grunt.registerTask('default', [ 'uglify', 'cssmin','processhtml', 'htmlmin', 'copy']);
 
 };
